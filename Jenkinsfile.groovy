5623ff51a6abc80f74f48e4ff07948846709a7b1
0xDONDArubin <102237672+0xDONDArubin@users.noreply.github.com>
2022-10-02 22:47:25 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,7 +18,7 @@ pipeline {
                                  followSymlinks: false
                 archiveArtifacts artifacts: 'CalcAdminTests/bin/Debug/netcoreapp3.1/*', 
                                  followSymlinks: false
-                sh dir
+                sh "dir"
             }
         }
     }
