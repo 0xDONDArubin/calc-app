422fb82fdea220f309a318cd8887a50231eeef21
0xDONDArubin <102237672+0xDONDArubin@users.noreply.github.com>
2022-10-10 20:05:12 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,10 +22,11 @@ pipeline {
             }
         }
         post {
-        always{
-           emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
-            Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
-                to: 'dondarubin@gmail.com'
+            always{
+               emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
+                Check console output at $BUILD_URL to view the results.''', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
+                    to: 'dondarubin@gmail.com'
+            }
         }
     }
 }
